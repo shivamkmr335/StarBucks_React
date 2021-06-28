b79ba6be48058f0efd767e907cd7c65b2e73eea7
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2021-06-28 10:19:11 +0530

    final chnages made to images

--- my-project/src/components/cashOrCard.js
+++ my-project/src/components/cashOrCard.js
@@ -16,14 +16,14 @@ function CashOrCard() {
             </div>
             <div className="md:flex">
                 <div className="flex py-6 w-full">
-                    <img className="w-28 " src="./images/1A.png" alt=""></img>
+                    <img className="w-28 " src="https://s1.im.ge/2021/06/28/rgtP8.png" alt=""></img>
                     <div className="ml-4">
                         <h3 className="font-semibold">Scan and pay seperately</h3>
                         <p className="mt-3 text-sm">Use cash or credit/debit card at the register.</p>
                     </div>
                 </div>
                 <div className="flex py-6 w-full">
-                    <img className="w-28 h-40" src="./images/1B.png" alt=""></img>
+                    <img className="w-28 h-40" src="https://s1.im.ge/2021/06/28/rgykh.png" alt=""></img>
                     <div className="ml-4">
                         <h3 className="font-semibold">Save payment in the app</h3>
                         <p className="mt-3 text-sm">Check-out faster by saving a credit/debit card or PayPal to your account. You’ll be able to order ahead or scan and pay at the register in one step.</p>
@@ -38,14 +38,14 @@ function CashOrCard() {
             </div>
             <div className="md:flex">
                 <div className="flex py-6 w-full">
-                    <img className="w-28 h-40" src="./images/2A.png" alt=""></img>
+                    <img className="w-28 h-40" src="https://s1.im.ge/2021/06/28/rgCFM.png" alt=""></img>
                     <div className="ml-4">
                         <h3 className="font-semibold">Preload</h3>
                         <p className="mt-3 text-sm">To save time and earn Stars twice as fast, add money to your digital Starbucks Card using any payment option. Scan and pay in one step or order ahead in the app.</p>
                     </div>
                 </div>
                 <div className="flex py-6 w-full">
-                    <img className="w-28 h-40" src="./images/2B.png" alt=""></img>
+                    <img className="w-28 h-40" src="https://s1.im.ge/2021/06/28/rgxRD.png" alt=""></img>
                     <div className="ml-4">
                         <h3 className="font-semibold">Register your gift card</h3>
                         <p className="mt-3 text-sm">Then use it to pay through the app. You can even consolidate balances from multiple cards in one place.</p>
@@ -61,7 +61,7 @@ function CashOrCard() {
             </div>
             <div>
                 <div className="flex py-6 md:w-1/2">
-                    <img className="w-28 h-40" src="./images/3A.png" alt=""></img>
+                    <img className="w-28 h-40" src="https://s1.im.ge/2021/06/28/rgE9Y.png" alt=""></img>
                     <div className="ml-4">
                         <h3 className="font-semibold">Earns Stars even faster</h3>
                         <p className="mt-3 text-sm">Earn Stars on all purchases you make with our <a href="https://www.starbucks.com/rewards" target="" className="text-green-700 underline">credit card</a> in and outside of Starbucks. Earn 1 Star per $1 when you digitally preload your Starbucks Card with your Starbucks® Rewards Visa® Card, and earn 2 Stars per $1 when you pay with that preloaded Starbucks Card.</p>
